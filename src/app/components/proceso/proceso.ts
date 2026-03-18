61503513e69d67800033e050c01c374cf454594a
Kevin Murillo <62905969+KevinMT5@users.noreply.github.com>
2026-03-17 21:56:52 -0600

    feat: initial NovuCode project setup

---
+++ src/app/components/proceso/proceso.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-proceso',
+  imports: [],
+  templateUrl: './proceso.html',
+  styleUrl: './proceso.scss',
+})
+export class Proceso {}
